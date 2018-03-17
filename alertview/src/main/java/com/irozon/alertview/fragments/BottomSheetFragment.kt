3b1003242c65e16b1d20d5803e77db699837142e
MD Danish Ansari <ansarid567@gmail.com>
2018-03-17 15:09:26 +0530

    Update BottomSheetFragment.kt

--- alertview/src/main/java/com/irozon/alertview/fragments/BottomSheetFragment.kt
+++ alertview/src/main/java/com/irozon/alertview/fragments/BottomSheetFragment.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialogFragment
 import android.support.v4.content.ContextCompat
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
